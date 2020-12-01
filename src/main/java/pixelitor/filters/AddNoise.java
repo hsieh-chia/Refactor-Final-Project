1bce676fa9e53deef84e4164fe1331924baa778e
Li-HC <p76091593@gs.ncku.edu.tw>
2020-12-01 15:55:44 +0800

    test

--- src/main/java/pixelitor/filters/AddNoise.java
+++ src/main/java/pixelitor/filters/AddNoise.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Pixelitor. If not, see <http://www.gnu.org/licenses/>.
  */
-
+/***8787***/
 package pixelitor.filters;
 
 import com.jhlabs.image.ImageMath;
