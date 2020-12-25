daf55553eda82c703c992befcdcb4e6de931831d
Li-HC <p76091593@gs.ncku.edu.tw>
2020-12-25 17:27:34 +0800

    fix the error of convolveV.java convolveH.java convolveHV.java IConvolve.java

--- src/main/java/com/jhlabs/image/IConvolve.java
+++ src/main/java/com/jhlabs/image/IConvolve.java
@@ -1,3 +1,10 @@
+package com.jhlabs.image;
+
+import java.awt.image.Kernel;
+
 public interface IConvolve {
+	public static final int CLAMP_EDGES = 1;
+	public static final int WRAP_EDGES = 2;
+
     public void convolveProcessing(Kernel kernel, int[] inputPixels, int[] outputPixels, int width, int height, boolean alpha, int edgeAction);
 }
