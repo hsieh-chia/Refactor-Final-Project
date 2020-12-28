f5e703dba0404e5cd36855ad6d3d44247225c2f7
Li-HC <p76091593@gs.ncku.edu.tw>
2020-12-28 15:52:27 +0800

    add the factory class: GradientBlendFactory.java

---
+++ src/main/java/com/jhlabs/image/GradientBlendFactory.java
@@ -0,0 +1,35 @@
+package com.jhlabs.image;
+
+import java.awt.Color;
+
+public class GradientBlendFactory{
+	private GradientBlend gradientblend;
+	public static final int LINEAR = 0x10;
+	public static final int SPLINE = 0x20;
+	public static final int CIRCLE_UP = 0x30;
+	public static final int CIRCLE_DOWN = 0x40;
+	public static final int CONSTANT = 0x50;
+	
+	public int GradientBlendOperation(int t,int blend){
+		switch (blend) {
+	        case CONSTANT:
+	            gradientblend = new ConstantGradientBlend();
+	            break;
+	        case LINEAR:
+	        	gradientblend = new LinearGradientBlend();
+	            break;
+	        case SPLINE:
+	            gradientblend = new SplineGradientBlend();
+	            break;
+	        case CIRCLE_UP:
+	            gradientblend = new CircleUpGradientBlend();
+	            break;
+	        case CIRCLE_DOWN:
+	            gradientblend = new CircleDownGradientBlend();
+	            break;
+	    }
+	    return gradientblend.GradientBlendOperation(t);
+	}
+}
+// GradientBlendFactory gradientblendfactory = new GradientBlendFactory();
+// t=gradientblendfactory.GradientBlendOperation(t,blend);
